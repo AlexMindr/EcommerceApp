1446213ab06aeac2cb2151fc45c382ee35036350
AlexMindr <49548048+AlexMindr@users.noreply.github.com>
2023-04-11 22:37:00 +0300

    added image assets and carousel

--- client/src/scenes/home/index.tsx
+++ client/src/scenes/home/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import MainCarousel from "./MainCarousel";
 
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <MainCarousel />
+    </div>
+  );
 };
 
 export default Home;
